00413a014a15eecd40d48dc048cdb81be30a90a8
Md Washi Ul Hoque <washi.washi786@gmail.com>
2023-06-26 21:03:54 +0530

    Adding dynamodb table

--- lib/cdk-data-stack.ts
+++ lib/cdk-data-stack.ts
@@ -1,7 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
+import {AttributeType, Table} from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 export class DataStack extends cdk.Stack{
     constructor(scope: Construct, id: string, props?: cdk.StackProps){
         super(scope, id, props);
+        
+        new Table(this, 'DynamoDB Stack',{
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            }
+        })
     }
 }
